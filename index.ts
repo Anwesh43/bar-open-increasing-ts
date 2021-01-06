f14a8a7a57d5dc75e58eb63ef97448be6774ba59
Anwesh43 <anweshthecool0@gmail.com>
2021-01-07 01:40:45 +0530

    adding BarOpenIncreasing class

--- index.ts
+++ index.ts
@@ -188,3 +188,26 @@ class BOINode {
         return this 
     }
 }
+
+class BarOpenIncreasing {
+
+    curr : BOINode = new BOINode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
