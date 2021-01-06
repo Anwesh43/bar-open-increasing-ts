5d2795e980de1fc429591da7172d21ed412a50ac
Anwesh43 <anweshthecool0@gmail.com>
2021-01-07 01:34:54 +0530

    adding BOINode class

--- index.ts
+++ index.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class BOINode {
+
+    next : BOINode 
+    prev : BOINode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BOINode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBOINode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BOINode {
+        var curr : BOINode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
